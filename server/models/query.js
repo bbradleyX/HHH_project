3d549e04443c01677e2be9d6f3d1128cbb577ffc
bbradleyX <bbradley@ithaca.edu>
2020-07-14 15:51:01 -0400

    updates to collection

--- server/models/query.js
+++ server/models/query.js
@@ -1,21 +1,76 @@
 const express = require ('express')
 const Users = require('../models/users');
+const Contacts = require ('../models/users/ContactSchema');
+//const Contacts = require ('../models/users');
 
 
 //this function gets a list of all the users
 //will pass back either data or the error message to the callback function
 const getUsers = (callback) => {
-  Users.find({}, 'name', callback)
+	try{
+		Users.find({}, 'name', callback)
+		Users.find({}, 'last_name', callback)
+		Users.find({}, 'email', callback)
+		Users.find({}, 'phone', callback)
+
+	}
+	catch{
+		console.error("Please fill in all required information");
+	}
 };
 
-const addUser = (param, callback) => {
-	Users.create(param, callback)
+const editUsers = (param, callback) => {
+	Users.update(param, callback)
+	Users.update(param, callback)
 };
+
+const deleteUsers = (param, callback) => {
+	Contacts.create(param, callback)
+};
+
+//-------------------------------------------------------------Split----------------------------------------------------------------
+
+const getContacts = (callback) => {
+	try{
+		Contacts.find({}, 'name', callback)
+		Contacts.find({}, 'last_name', callback)
+		Contacts.find({}, 'email', callback)
+		Contacts.find({}, 'phone', callback)
+
+	}
+	catch{
+		console.error("Please fill in all required information");
+	}
+};
+
+const addContacts = (param, callback) => {
+	Contacts.create(param, callback)
+};
+
+const editContacts = (param, callback) => {
+	Contacts.update(name, callback)
+	Contacts.update(last_name, callback)
+	Contacts.update(, callback)
+	Contacts.update(lastname, callback)
+};
+
+const deleteContacts = (param, callback) => {
+	Contacts.create(param, callback)
+};
+
 
 
 const queries = {
+	//User Functions
 	getUsers: getUsers,
-	addUser: addUser
+	editUsers: editUsers,
+	deleteUsers: deleteUsers,
+
+	//Contact Functions
+	getContacts: getContacts,
+	addContacts: addContacts,
+	editContacts: editContacts,
+	deleteContacts: deleteContacts
 }
 
 module.exports = queries;
